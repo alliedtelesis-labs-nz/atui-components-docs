9b6c5403ccff6b4581d701242cd99f4304f7a077
rikis-ATL <riki.sommers@alliedtelesis.co.nz>
2025-07-28 23:51:57 +0000

    Deploying to main from @ alliedtelesis-labs-nz/atui-components@6cdb99790062a526a0a1e65169c9032fbdbb2976 🚀

---
+++ storybook/assets/atui-color-status-cell-_lYM2geV.js
@@ -0,0 +1,10 @@
+import{j as t,M as r}from"./index-ChmaFPCR.js";import{useMDXComponents as s}from"./index-orqQb_IM.js";import{ar as a}from"./MermaidMarkdown-CchJh94D.js";import"./iframe-M1nTw5cZ.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";const l=`# atui-color-status-cell
+
+<!-- Auto Generated Below -->
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
+`;function e(n){return t.jsxs(t.Fragment,{children:[t.jsx(r,{title:"Components/Table Components/Cell Components/Color Status Cell/Color Status Cell"}),`
+`,t.jsx(a,{md:l})]})}function d(n={}){const{wrapper:o}={...s(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e()}export{d as default};
