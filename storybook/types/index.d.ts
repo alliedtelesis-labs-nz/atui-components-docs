da15f5136c86ca1d6710517a3689c147c1330824
jamesc-at <james.chalmers@alliedtelesis.co.nz>
2026-04-16 23:38:12 +0000

    Deploying to main from @ alliedtelesis-labs-nz/atui-components@79a4b3e19ab8b6209131b2f86da0c23c5a84081c 🚀

--- storybook/types/index.d.ts
+++ storybook/types/index.d.ts
@@ -31,6 +31,7 @@ export { AtITab } from './components/at-tabs/at-tabs';
 export { AtISearchTableParams, AtISearchData, } from './types/generic-table/index';
 export { AtBadgeType } from './components/at-badge/at-badge';
 export { AtIRadioOption } from './components/at-radio-group/at-radio-group';
+export { AtIButtonGroupOption } from './components/at-button-group/at-button-group';
 export { AtTimeDateUtil } from './utils/at-time-date.util';
 export { type AtSelectedTimeRangeExtended, AtTimePresets, } from './models/at-time-range.models';
 export { type AtIDateRange, type AtIDateRangeStrings } from './types/date';
