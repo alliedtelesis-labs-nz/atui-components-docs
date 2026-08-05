724185105bc97ae846d362ad49456e8c7e33f680
rikis-ATL <riki.sommers@alliedtelesis.co.nz>
2026-08-05 22:49:15 +0000

    Deploying to main from @ alliedtelesis-labs-nz/atui-components@0eed2616b6524aafa869db40f0a5a00d60fe1ba5 🚀

--- storybook/cjs/at-tab-content.cjs.entry.js
+++ storybook/cjs/at-tab-content.cjs.entry.js
@@ -30,7 +30,7 @@ const AtTabContent = class {
     }
     disconnectedCallback() {
         if (this.tabSelector) {
-            this.tabSelector.removeEventListener('atuiChangeId', this.updateActiveState);
+            this.tabSelector.removeEventListener('atuiTabChange', this.updateActiveState);
         }
     }
     setIsActive(id) {
@@ -41,7 +41,7 @@ const AtTabContent = class {
         this.isActive = selectedTabId === this.tab_id;
     };
     render() {
-        return (index.h("div", { key: '84b1257e823d572872e34fad3e2a7309f9446858', class: `${this.isActive ? 'flex flex-col focus-visible:outline-none' : 'hidden'}`, role: "tabpanel", id: `panel-${this.tab_id}`, "aria-labelledby": `tab-${this.tab_id}`, tabIndex: this.isActive ? 0 : -1, "aria-hidden": !this.isActive }, index.h("slot", { key: 'd7b2f85fde978110d597bb99d241d8cdd2392b3d' })));
+        return (index.h("div", { key: '6eb8ea1f22ab4d9d25c186e23a01fc21b95def16', class: `${this.isActive ? 'flex flex-col focus-visible:outline-none' : 'hidden'}`, role: "tabpanel", id: `panel-${this.tab_id}`, "aria-labelledby": `tab-${this.tab_id}`, tabIndex: this.isActive ? 0 : -1, "aria-hidden": !this.isActive }, index.h("slot", { key: 'c57ffa58e656f4ba734bc027ac48c1bbf5084381' })));
     }
 };
 
